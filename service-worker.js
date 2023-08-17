127b992faca9c4eeb4e8664e6b1e3beac47a1b83
Spike <syedibrahim1388@gmail.com>
2023-08-16 21:58:56 -0400

    updated service worker name

--- service-worker.js
+++ service-worker.js
@@ -3,10 +3,10 @@ self.addEventListener('install', (event) => {
     caches.open('my-game-cache').then((cache) => {
       return cache.addAll([
         './',
-        './Build/v2.2.1 Mobile FIxes plus rp.data',
-        './Build/v2.2.1 Mobile FIxes plus rp.framework',
-        './Build/v2.2.1 Mobile FIxes plus rp.loader',
-        './Build/v2.2.1 Mobile FIxes plus rp.wasm',
+        './Build/v2.2.1%20Mobile%20FIxes%20plus%20rp.data',
+        './Build/v2.2.1%20Mobile%20FIxes%20plus%20rp.framework',
+        './Build/v2.2.1%20Mobile%20FIxes%20plus%20rp.loader',
+        './Build/v2.2.1%20Mobile%20FIxes%20plus%20rp.wasm',
       ]);
     })
   );
